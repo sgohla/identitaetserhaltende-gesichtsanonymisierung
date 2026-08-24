b8e216bf37133b620a8d66c02984d19e8af35e64
Ihr Name <Ihre E-Mail-Adresse>
2026-08-24 20:35:26 +0200

    parameter geändert

--- mot_eval_vlm.py
+++ mot_eval_vlm.py
@@ -423,7 +423,7 @@ MAX_LOST_FRAMES_FOR_RELINKING = 200  # Maximale Anzahl an Frames, die ein Track 
 person_histogram_models = {}  #geglättetes Histogramm
 HISTOGRAM_ALPHA = 0.15
 
-MIN_HISTOGRAM_SIMILARITY= 0.55
+MIN_HISTOGRAM_SIMILARITY= 0.60
 MIN_SCORE_MARGIN = 0.10
 MIN_SIMILARITY_MARGIN = 0.08
 MAX_POSITION_DISTANCE_FACTOR = 1.5 
@@ -433,7 +433,7 @@ relinked_track_ids = set()
 
 
 pending_track_frames ={}                 # paar frames abwarten bis zum Relinking, damit Histogramm aussagekräftig ist
-MIN_FRAMES_BEFORE_RELINKING = 3
+MIN_FRAMES_BEFORE_RELINKING = 5
 PENDING_HISTOGRAM_ALPHA = 0.30
 
 
